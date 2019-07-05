4d48c8582e4a92644e671f2efc5d1c88aed7467d
toshyamsundar <toshyamsundar@yahoo.com>
2019-07-04 23:31:16 -0400

    Updates

---
+++ precache-manifest.2b70450b24036ccbc25517166c048cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa972d2d08dd3b8c839f41f42a493b5a",
+    "url": "/toshyamsundar/books-search/index.html"
+  },
+  {
+    "revision": "0012fc4b5f0d864b40d6",
+    "url": "/toshyamsundar/books-search/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/toshyamsundar/books-search/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0012fc4b5f0d864b40d6",
+    "url": "/toshyamsundar/books-search/static/js/main.86165448.chunk.js"
+  },
+  {
+    "revision": "9954d96a0e4479bf2f81",
+    "url": "/toshyamsundar/books-search/static/js/runtime~main.93504289.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toshyamsundar/books-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
